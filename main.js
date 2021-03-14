d30b21ed9f9750339b3432ce46b9870a0230d99c
syler11 <77334233+syler11@users.noreply.github.com>
2021-03-14 12:01:58 +0000

    Sort function added

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-let words = ['apple', 'smile'];
+
 
 function addWords() {
     let newWord = words.map(word => `<li>${word}</li>`).join('\n');
     document.getElementById('easy').innerHTML = newWord;
 }
 
-addWords();
 
 let btnAdd = document.getElementById('btnAdd');
 let inputWord = document.getElementById('inputWord');
@@ -26,3 +25,24 @@ btnDelete.addEventListener('click', () =>{
     inputDelete.value = '';
     addWords();
 });
+
+let words = ['apple', 'call', 'test','smile'];
+
+addWords();
+
+word_four = [];
+for(var i=0; i<words.length; i++){
+    if(words[i].length == 4) 
+        word_four.push(words[i]);
+}
+
+document.getElementById('medium').innerHTML = (word_four).join("<br/>");
+
+word_five = [];
+for(var i=0; i<words.length; i++){
+    if(words[i].length == 5) 
+        word_five.push(words[i]);
+}
+
+document.getElementById('hard').innerHTML = (word_five).join("<br/>");
+
